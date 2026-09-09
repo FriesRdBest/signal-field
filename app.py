174cb0d2fd0ded7e09cdbc3dd9dcedfd097bae6e
FriesRdBest <sylvester.career@gmail.com>
2026-09-09 04:52:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Signal Field",
-    page_icon="◌",
+    page_icon="https://corp.dd-static.net/img/favicons/dd-favicon.png",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
